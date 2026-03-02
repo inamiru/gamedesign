c83a998cb5ae5b8dceff074600a26c6761ee9853
inamiru <gc_stay@icloud.com>
2026-03-02 09:22:11 +0900

    Update manifest.js

--- js/sessions/manifest.js
+++ js/sessions/manifest.js
@@ -1,7 +1,7 @@
 // js/sessions/manifest.js
 // セッションの並び順を「ここだけ」で管理
 window.SESSION_MANIFEST = {
-  homeTitle: '目次へ',
+  homeTitle: '一覧へ',
   homePath: '../', // 各セッションHTML(UXdesign/XXSession/)から見てUXdesign/へ
 
   // 表示名と、セッションの「識別子（id）」を並べる
