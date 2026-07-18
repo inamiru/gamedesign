822c49334afdf1b307e94ddf6bf8ebc401949654
inamiru <gc_stay@icloud.com>
2026-07-18 11:37:58 +0900

    Add second session to manifest

--- js/sessions/manifest.js
+++ js/sessions/manifest.js
@@ -45,6 +45,7 @@ window.SESSION_MANIFEST = {
       homePath: '../',
       sessions: [
         { sessionId: 'ld-1', title: '第1回', path: '../FirstSession/' },
+        { sessionId: 'ld-2', title: '第2回', path: '../SecondSession/' },
       ],
     },
   ],
